b402f531ac77c5afa675bc90989d51910f1d58b7
grangec <grangec@free.fr>
2021-02-26 19:16:06 +0100

    Nettoyage Code.

--- videoRename.php
+++ videoRename.php
@@ -4,9 +4,8 @@
 $logLevel=0; // niveau général de log
 
 function setGlobalLogLevel($level) {
-    llog("Log Level = " . $level);
     global $logLevel;
-    $logLevel=$level;    
+    $logLevel=$level;
 }
 
 function getGlobalLogLevel() {
@@ -14,10 +13,38 @@ function getGlobalLogLevel() {
     return $logLevel;
 }
 
-function llog($ch,$level=0) {
+function llog($ch="",$level=0) {
     if($level<=getGlobalLogLevel()) {
         echo $ch ."\n";
     }
+}
+
+function init() {
+    $params=getopt("b::h::R::v::d:");
+    
+    if(key_exists("h", $params)) {
+        llog("==============");
+        llog(" videoRenamer");
+        llog("==============");
+        llog();
+        llog("-d <directory> : The Directory containing video files to rename.");
+        llog("-R             : Recursivly.");
+        llog("-v             : Verbose");
+        llog("-b             : Debug");
+        llog("-h             : Help");
+        llog();
+        llog("02/2021 : https://github.com/grangec/videoRenamer");
+        exit(0);
+    }
+    
+    setGlobalLogLevel(key_exists("v", $params)?1:0);
+    
+    if(key_exists("b", $params)) {
+        llog("Debug activé.");
+        setGlobalLogLevel(2);
+    }
+    
+    return $params;
 }
 
 function isDir ($file)
@@ -27,7 +54,7 @@ function isDir ($file)
 }
 
 function videoRenameFile($dirname,$filename) {
-    llog("videoRenameFile:" . $dirname . "-".$filename,1);
+    llog("videoRenameFile:" . $dirname . "-".$filename,2);
     $fileRenamed = [
         "dir" => $dirname,
         "name" => $filename,
@@ -38,11 +65,11 @@ function videoRenameFile($dirname,$filename) {
 }
 
 function videoRenameDir($dirname,$recurs) {
-    llog("videoRenameDir:" . $dirname,1);
+    llog("videoRenameDir:" . $dirname,2);
     $d = dir($dirname);
     $filesRenamed=[];
     while (false !== ($entry = $d->read())) {
-        llog("Parcours : " . $entry,1);
+        llog("Parcours : " . $entry,2);
         if($entry!="." && $entry != "..") {
             if(isDir($dirname .$entry) && $recurs) {
                 $filesRenamed=array_merge($filesRenamed,videoRenameDir($dirname . $entry,$recurs));
@@ -57,7 +84,7 @@ function videoRenameDir($dirname,$recurs) {
 }
 
 function fRealPath($dirname) {
-    llog("fRealPath : " . $dirname,1);
+    llog("fRealPath : " . $dirname,2);
     $rpath="";
     if(substr($dirname, 0, 1)=="~") {
         $rpath=getenv("HOME").substr($dirname, 1);        
@@ -67,30 +94,31 @@ function fRealPath($dirname) {
     return $rpath;
 }
 
-$params=getopt("R::v::d:");
+// Main
+$params=init();
+
+// Traitement
 if(key_exists("d", $params) && $params["d"]) {
     $filesRenamedTab=[];
     
-    $debug=key_exists("v", $params);
-    setGlobalLogLevel($debug?1:0);
-    
     $dirname=fRealPath($params["d"]);
     if(isDir($dirname)) {
-        llog("Traitement repertoire : " . $dirname,1);
+        llog("Traitement repertoire : " . $dirname,2);
         $dirname=$dirname.(substr($dirname,-1)=="/"?"":"/");
         $recurs=key_exists("R",$params);
         $filesRenamedTab=videoRenameDir($dirname,$recurs);
     } elseif (is_file($dirname)) {
-        llog("Traitement fichier : " . $dirname,1);
+        llog("Traitement fichier : " . $dirname,2);
         $filesRenamedTab[$dirname]=videoRenameFile($dirname);
     } else {
         llog("'".$dirname . "' n'est pas un dossier");
         exit(2);
     }
-    llog("Tableau des fichier renommés :");
-    llog(json_encode($filesRenamedTab,JSON_PRETTY_PRINT));
+    llog("Tableau des fichier renommés :",2);
+    llog(json_encode($filesRenamedTab,JSON_PRETTY_PRINT),1);
 } else {
     llog("Vous devez préciser un dossier (option -d)");
     exit(1);
 }
+
 exit(0);
